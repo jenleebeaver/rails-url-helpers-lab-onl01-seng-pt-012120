acd86fb8a081ab122278188415231bc63a406834
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-05-27 17:43:03 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200527031153) do
+ActiveRecord::Schema.define(version: 20200527174243) do
 
   create_table "students", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "active"
+    t.         "default"
   end
 
 end
