df68ea15968c4215ae331672787581801275a12a
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-05-27 17:44:36 +0000

    Automatically backed up by Learn

--- db/migrate/20200527174243_add_active_to_students.rb
+++ db/migrate/20200527174243_add_active_to_students.rb
@@ -1,6 +1,5 @@
 class AddActiveToStudents < ActiveRecord::Migration[5.0]
   def change
-    add_column :students, :active, :boolean
-    add_column :students, :default, :false
+    add_column :students, :active, :boolean, :default, :false
   end
 end
